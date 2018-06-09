a7fa719fe55275efd2ebb2461a56541197c66cae
Hugo Scurti <hugoscurti@gmail.com>
2018-06-09 18:52:33 -0400

    Added Demo scene; Added scripts to experiment with cutting;

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Adapted from
+/// https://answers.unity.com/questions/1257281/how-to-rotate-camera-orbit-around-a-game-object-on.html
+/// </summary>
+
+public class CameraOrbit : MonoBehaviour {
+
+    public Transform target;
+
+    public float distance = 2.0f;
+    public float xSpeed = 5.0f;
+    public float ySpeed = 5.0f;
+    public float yMinLimit = -90f;
+    public float yMaxLimit = 90f;
+    public float distanceMin = 2f;
+    public float distanceMax = 10f;
+    float rotationYAxis = 0.0f;
+    float rotationXAxis = 0.0f;
+ 
+
+    // Use this for initialization
+    void Start () {
+
+        Vector3 angles = transform.eulerAngles;
+        rotationYAxis = angles.y;
+        rotationXAxis = angles.x;
+
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+
+    // Called after Update
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                rotationYAxis += xSpeed * Input.GetAxis("Mouse X") * distance;
+                rotationXAxis -= ySpeed * Input.GetAxis("Mouse Y");
+                rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+            }
+
+            Quaternion toRotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+            Quaternion rotation = toRotation;
+
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+            RaycastHit hit;
+            if (Physics.Linecast(target.position, transform.position, out hit))
+            {
+                distance -= hit.distance;
+            }
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+
+}
---
+++ Assets/Scripts/MouseSlice.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSlice : MonoBehaviour {
+
+    bool dragging;
+    Vector3 start;
+    Vector3 end;
+
+    LineRenderer line;
+
+    public GameObject plane;
+
+    Ray mouseRay;
+    readonly float distanceFromNearPlane = 2;
+
+	// Use this for initialization
+	void Start () {
+        dragging = false;
+
+        line = GetComponent<LineRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        //Debug.Log(Camera.main.transform.forward);
+        if (Input.GetMouseButtonDown(0))
+        {
+            start = GetMousePosOnCamera();
+            //Debug.Log("Start: " + start);
+            line.SetPosition(0, start);
+            dragging = true;
+        } 
+
+        if (dragging)
+        {
+            line.SetPosition(1, GetMousePosOnCamera());
+        }
+
+        if (dragging && Input.GetMouseButtonUp(0))
+        {
+            end = GetMousePosOnCamera();
+            //Debug.Log("End: " + end);
+            line.SetPosition(1, end);
+            dragging = false;
+
+            var depthAxis = Camera.main.transform.forward;
+            var planeTangent = end - start;
+            var normalVec = Vector3.Cross(depthAxis, planeTangent);
+
+            //DrawPlane(normalVec);
+            Plane plane = new Plane(normalVec, start);
+        }
+    }
+
+    void DrawPlane(Vector3 normalVec)
+    {
+        Quaternion rotate = Quaternion.FromToRotation(Vector3.up, normalVec);
+
+        plane.transform.localRotation = rotate;
+        plane.transform.position = (end + start) / 2;
+        plane.SetActive(true);
+    }
+
+    Vector3 GetMousePosOnCamera()
+    {
+        var cam = Camera.main;
+        mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+        return mouseRay.GetPoint(cam.nearClipPlane + distanceFromNearPlane);
+    }
+}
