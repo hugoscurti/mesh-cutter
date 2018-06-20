0995b14a350303c9a0dff1e2790df1fe9a58ad96
Hugo Scurti <hugoscurti@gmail.com>
2018-06-20 15:24:43 -0400

    Completed slicing; Added tags;

--- Assets/Scripts/MouseSlice.cs
+++ Assets/Scripts/MouseSlice.cs
@@ -11,48 +11,15 @@ public class MouseSlice : MonoBehaviour {
     LineRenderer line;
 
     public GameObject plane;
+    public GameObject SlicedPrefab;
+    public Transform ObjectContainer;
+
+    public float separation;
 
     Ray mouseRay;
     readonly float distanceFromNearPlane = 2;
 
-	// Use this for initialization
-	void Start () {
-        dragging = false;
-
-        line = GetComponent<LineRenderer>();
-	}
-
-    // Update is called once per frame
-    void Update() {
-        //Debug.Log(Camera.main.transform.forward);
-        if (Input.GetMouseButtonDown(0))
-        {
-            start = GetMousePosOnCamera();
-            //Debug.Log("Start: " + start);
-            line.SetPosition(0, start);
-            dragging = true;
-        } 
-
-        if (dragging)
-        {
-            line.SetPosition(1, GetMousePosOnCamera());
-        }
-
-        if (dragging && Input.GetMouseButtonUp(0))
-        {
-            end = GetMousePosOnCamera();
-            //Debug.Log("End: " + end);
-            line.SetPosition(1, end);
-            dragging = false;
-
-            var depthAxis = Camera.main.transform.forward;
-            var planeTangent = end - start;
-            var normalVec = Vector3.Cross(depthAxis, planeTangent);
-
-            //DrawPlane(normalVec);
-            Plane plane = new Plane(normalVec, start);
-        }
-    }
+    #region Utility Functions
 
     void DrawPlane(Vector3 normalVec)
     {
@@ -68,5 +35,132 @@ public class MouseSlice : MonoBehaviour {
         var cam = Camera.main;
         mouseRay = cam.ScreenPointToRay(Input.mousePosition);
         return mouseRay.GetPoint(cam.nearClipPlane + distanceFromNearPlane);
+    }
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        dragging = false;
+
+        line = GetComponent<LineRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (!dragging && Input.GetMouseButtonDown(0))
+        {
+            start = GetMousePosOnCamera();
+            line.SetPosition(0, start);
+            dragging = true;
+        } 
+
+        if (dragging)
+        {
+            line.SetPosition(1, GetMousePosOnCamera());
+        }
+
+        if (dragging && Input.GetMouseButtonUp(0))
+        {
+            // Finished dragging. We draw the line segment
+            end = GetMousePosOnCamera();
+            line.SetPosition(1, end);
+            dragging = false;
+
+            //var depthAxis = Camera.main.transform.forward;
+            var depthAxis = mouseRay.direction.normalized;
+
+            var planeTangent = end - start;
+            var normalVec = Vector3.Cross(depthAxis, planeTangent);
+
+            DrawPlane(normalVec);
+            SliceObjects(start, normalVec);
+        }
+    }
+
+    void SliceObjects(Vector3 point, Vector3 normal)
+    {
+        Plane slicePlane = new Plane();
+        var toSlice = GameObject.FindGameObjectsWithTag("Sliceable");
+        GameObject obj;
+        for (int i = 0; i < toSlice.Length; ++i)
+        {
+            obj = toSlice[i];
+
+            //Convert plane in object's local frame
+            slicePlane.SetNormalAndPosition(
+                obj.transform.InverseTransformVector(normal),
+                obj.transform.InverseTransformPoint(point));
+
+            SliceObject(ref slicePlane, obj);
+        }
+    }
+
+    void SliceObject(ref Plane slicePlane, GameObject obj)
+    {
+        var mesh = obj.GetComponent<MeshFilter>().mesh;
+
+        TempMesh posMesh, negMesh;
+
+        if (!MeshCutter.SliceMesh(mesh, slicePlane, out posMesh, out negMesh))
+        {
+            // If we didn't slice the object then no need to separate it into 2 objects
+            Debug.Log("Didn't slice");
+            return;
+        }
+
+        // TODO: Update center of mass
+
+        // Put the bigger mesh in the original object
+        bool posBigger = posMesh.surfacearea > negMesh.surfacearea;
+        ReplaceMesh(mesh, (posBigger ? posMesh : negMesh), obj.GetComponent<MeshCollider>());
+
+        // Create new Sliced object with the other mesh
+        GameObject newObject = Instantiate(SlicedPrefab, ObjectContainer);
+        newObject.transform.SetPositionAndRotation(obj.transform.position, obj.transform.rotation);
+        var newObjMesh = newObject.GetComponent<MeshFilter>().mesh;
+        ReplaceMesh(newObjMesh, (posBigger ? negMesh : posMesh), newObject.GetComponent<MeshCollider>());
+
+        Transform posTransform, negTransform;
+        if (posBigger)
+        {
+            posTransform = obj.transform;
+            negTransform = newObject.transform;
+        } else
+        {
+            posTransform = newObject.transform;
+            negTransform = obj.transform;
+        }
+
+        // Separate meshes 
+        SeparateMeshes(posTransform, negTransform, slicePlane.normal);
+    }
+
+
+    /// <summary>
+    /// Replace the mesh with tempMesh.
+    /// </summary>
+    void ReplaceMesh(Mesh mesh, TempMesh tempMesh, MeshCollider collider = null)
+    {
+        mesh.Clear();
+        mesh.vertices = tempMesh.vertices.ToArray();
+        mesh.triangles = tempMesh.triangles.ToArray();
+        mesh.normals = tempMesh.normals.ToArray();
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateTangents();
+
+        if (collider != null)
+        {
+            collider.sharedMesh = mesh;
+            collider.convex = true;
+        }
+    }
+
+    void SeparateMeshes(Transform posTransform, Transform negTransform, Vector3 normal)
+    {
+        Vector3 separationVec = normal * separation;
+        posTransform.position += separationVec;
+        negTransform.position -= separationVec;
     }
 }
