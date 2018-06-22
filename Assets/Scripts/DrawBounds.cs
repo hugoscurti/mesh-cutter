df7779d525e8e613030f76373eee62762ee83589
Hugo Scurti <hugoscurti@gmail.com>
2018-06-22 15:39:14 -0400

    Added prefab for sliced objects; Added utility script to draw a mesh's bound in Scene view; Disabled colliders for now;

---
+++ Assets/Scripts/DrawBounds.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DrawBounds : MonoBehaviour {
+
+    private new MeshRenderer renderer;
+    private MeshFilter filter;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<MeshRenderer>();
+        filter = GetComponent<MeshFilter>();
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        if (renderer != null && enabled)
+        {
+            Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.matrix = rotationMatrix;
+            Gizmos.DrawWireCube(filter.mesh.bounds.center, filter.mesh.bounds.size);
+
+            // This is the renderer's bounds, which is less accurate if we account rotations
+            //Gizmos.DrawWireCube(renderer.bounds.center, renderer.bounds.size);
+        }
+
+
+
+    }
+}
