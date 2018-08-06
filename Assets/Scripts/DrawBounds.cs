52add43ead86d9a877d76ce311a48f0e28c155c1
Hugo Scurti <hugoscurti@gmail.com>
2018-08-06 12:01:34 -0400

    Simplified DrawBounds so that it stops throwing error when filter is not initialized;

--- Assets/Scripts/DrawBounds.cs
+++ Assets/Scripts/DrawBounds.cs
@@ -2,29 +2,17 @@
 
 public class DrawBounds : MonoBehaviour {
 
-    private new MeshRenderer renderer;
     private MeshFilter filter;
-
-	// Use this for initialization
-	void Start () {
-        renderer = GetComponent<MeshRenderer>();
-        filter = GetComponent<MeshFilter>();
-	}
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        if (renderer != null && enabled)
+        if (enabled)
         {
-            Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
-            Gizmos.matrix = rotationMatrix;
-            Gizmos.DrawWireCube(filter.mesh.bounds.center, filter.mesh.bounds.size);
-
-            // This is the renderer's bounds, which is less accurate if we account rotations
-            //Gizmos.DrawWireCube(renderer.bounds.center, renderer.bounds.size);
+            if (!filter) filter = GetComponent<MeshFilter>();
+            var modelMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            Gizmos.matrix = modelMatrix;
+            Gizmos.DrawWireCube(filter.sharedMesh.bounds.center, filter.sharedMesh.bounds.size);
         }
-
-
-
     }
 }
